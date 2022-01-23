c086c82a76f7098dc2c3b840f2eb0689776dfc80
Metalheadpnz <metalheadpnz@gmail.com>
2022-01-23 23:27:57 +0300

    13. Creation template

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+
+            <div><a href="">profile</a></div>
+            <div><a href="">messages</a></div>
+            <div><a href="">news</a></div>
+            <div><a href="">music</a></div>
+            <div><a href="">settings</a></div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://fs.tonkosti.ru/sized/c960x400/3q/3r/3q3r3fgg4544os4c440gkskgg.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
