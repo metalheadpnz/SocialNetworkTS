de032ec4ab90e76b446cd0eecc631c2d54022913
Metalheadpnz <metalheadpnz@gmail.com>
2022-01-24 00:10:28 +0300

    14. CSS modules

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav className='nav'>
 
-            <div><a href="">profile</a></div>
+            <div><a href="" className={styles.test}>profile</a></div>
             <div><a href="">messages</a></div>
             <div><a href="">news</a></div>
             <div><a href="">music</a></div>
