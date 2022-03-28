1066bc6693d7d3c5671055a61c78c81f22fbf4a0
Metalheadpnz <metalheadpnz@gmail.com>
2022-03-28 18:32:47 +0300

    20. NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar: React.FC = () => {
+    const NavLinksClassName = (p: { isActive: boolean }) => p.isActive ? `${s.item} ${s.active}` : s.item
     return (
         <nav className={s.nav}>
-            <div><a href="/profile" className={`${s.item} ${s.active}`}>profile</a></div>
-            <div><a href="/dialogs" className={s.item}>messages</a></div>
-            <div><a href="" className={s.item}>news</a></div>
-            <div><a href="" className={s.item}>music</a></div>
-            <div><a href="" className={s.item}>settings</a></div>
+            <div className={s.navLinkWrap}>
+                <NavLink to="/profile" className={NavLinksClassName}>Profile</NavLink>
+            </div>
+            <div className={s.navLinkWrap}>
+                <NavLink to="/dialogs" className={NavLinksClassName}>Dialogs</NavLink>
+            </div>
+            <div className={s.navLinkWrap}>
+                <NavLink to="/news" className={NavLinksClassName}>News</NavLink>
+            </div>
+            <div className={s.navLinkWrap}>
+                <NavLink to="/music" className={NavLinksClassName}>Music</NavLink>
+            </div>
+            <div className={s.navLinkWrap}>
+                <NavLink to="/settings" className={NavLinksClassName}>Settings</NavLink>
+            </div>
         </nav>
     );
 };
