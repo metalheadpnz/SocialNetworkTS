820a627ddf8b72ebbab1ef27dfe3789b13305754
Metalheadpnz <metalheadpnz@gmail.com>
2022-03-28 19:32:18 +0300

    21. Dialogs page layout creating

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
-const Dialogs = () => {
+const Dialogs: React.FC = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogsPageWrap}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialogs}>Mark</div>
+                <div className={s.dialogs}>Elon</div>
+                <div className={s.dialogs}>Linus</div>
+                <div className={s.dialogs}>Dymich</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>ZX-Spectrum is cool!</div>
+                <div className={s.message}>ATMega328 is better then ATiny13</div>
+            </div>
+
+
         </div>
     );
 };
